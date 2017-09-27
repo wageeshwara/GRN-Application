326877e6a049e33b8f87b3878c81ce53409e06f1
suranasinghe <suranasinghe@CMDSURANASINGHE>
2017-09-27 12:28:18 +0530

    Add project files.

---
+++ GRNApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GRNApplication.Startup))]
+namespace GRNApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
